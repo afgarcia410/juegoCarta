374ec93e005548070bc0a8a8f3841e146f91e98e
afgarcia410 <115699382+afgarcia410@users.noreply.github.com>
2023-04-17 13:55:25 +0200

    Añadido filtro para numero

--- js/baraja.js
+++ js/baraja.js
@@ -2,23 +2,25 @@ import { Carta } from "./carta.js";
 class Baraja {
   constructor() {
     this.baraja = [];
-    let palosBaraja = ["Bastos", "Copas", "Espadas", "Oros"];
-    let numerosBaraja = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
+    const palosBaraja = ["Bastos", "Copas", "Espadas", "Oros"];
+    const numerosBaraja = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
      /*
     for (let i = 0; i < palosBaraja.length; i++) {
       for (let j = 0; j < numerosBaraja.length; j++) {
         this.baraja.push(new Carta(i, j));
       }
     }*/
+    const filtroNumero = numerosBaraja.filter((valor) =>{
+      return valor !=8 && valor !=9;
+    });
     palosBaraja.forEach((palos) => {
-      numerosBaraja.forEach((numeros) => {
+      filtroNumero.forEach((numeros) => {
         this.baraja.push(new Carta(palos, numeros));
       });
     });
+    
   }
-  
 }
-let barajass = new Baraja();
-console.log(barajass);
+let barajas = new Baraja();
+console.log(barajas);
 export { Baraja };
